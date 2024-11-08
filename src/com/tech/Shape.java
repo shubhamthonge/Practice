a4a27c447d32317e2fb8327bd0330aa647e0e1dc
shubhamthonge <shubhamthonge131@gmail.com>
2024-11-08 11:50:13 +0530

    add19

---
+++ src/com/tech/Shape.java
@@ -0,0 +1,11 @@
+package com.tech;
+
+
+// Abstract class Shape
+public abstract class Shape {
+	protected int length, breadth, height;
+
+	// Abstract method for calculating volume
+	public abstract int calcArea();
+}
+
